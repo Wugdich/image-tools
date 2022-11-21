a1271dabf1a393bd84d22c84ffb059086effba7e
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-11-21 17:22:19 +0300

    Add createing rgb squares wallpaper function

--- img_tools.py
+++ img_tools.py
@@ -1,4 +1,39 @@
+from time import localtime, strftime
+
+import numpy as np
 from PIL import Image, ImageFilter
+
+
+def create_rgb_wallpaper(resolution: tuple) -> None:
+    height, width = resolution
+    red = np.zeros(resolution)
+    green = np.zeros(resolution)
+    blue = np.zeros(resolution)
+
+    # Green square pixels. This square places in center so it's pixels
+    # used as offset base for two another squares.
+    gp = [
+            height // 2 - 100, height // 2 + 100,
+            width // 2 - 100, width // 2 + 100,
+            ]
+    # Red square pixels.
+    rp = [p - 50 for p in gp]
+    # Blue square pixels.
+    bp = [p + 50 for p in gp]
+
+    red[rp[0]:rp[1], rp[2]:rp[3]] = 255
+    green[gp[0]:gp[1], gp[2]:gp[3]] = 255
+    blue[bp[0]:bp[1], bp[2]:bp[3]] = 255
+
+    red_img = Image.fromarray(red).convert("L")
+    green_img = Image.fromarray(green).convert("L")
+    blue_img = Image.fromarray(blue).convert("L")
+
+    final_img = Image.merge("RGB", (red_img, green_img, blue_img))
+    uniq_number = strftime("%Y%m%d%H%M%S", localtime())
+    final_img_filename = f"./images/redgreenblue{uniq_number}.jpg"
+    final_img.save(final_img_filename)
+    print(f"Image saved to {final_img_filename}")
 
 
 def tile(*images: Image.Image, vertical:bool=False) -> Image.Image:
@@ -31,24 +66,8 @@ def dilate(cycles: int, image: Image.Image) -> Image.Image:
 
 
 def main() -> None:
-    filename = "./images/testing-img.jpg"
-    with Image.open(filename) as img:
-        img.load()
-
-
-    red, green, blue = img.split()
-    zeroes = red.point(lambda _: 0)
-    red_merge = Image.merge(
-            "RGB", (red, zeroes, zeroes)
-            )
-    green_merge = Image.merge(
-            "RGB", (zeroes, green, zeroes)
-            )
-    blue_merge = Image.merge(
-            "RGB", (zeroes, zeroes, blue)
-            )
-    tiled_img = tile(red_merge, green_merge, blue_merge)
-    tiled_img.show()
+    resolution = (2_532, 1_170)
+    create_rgb_wallpaper(resolution)
 
 
 if __name__ == '__main__':
